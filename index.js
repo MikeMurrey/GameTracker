23a25c533c21656d9bdfb3dc28edaf1423ddc7b7
MikeMurrey <108901311+MikeMurrey@users.noreply.github.com>
2023-04-17 13:34:13 -0600

    Add user model and passport to project
    user model created.  Passport, passport-local and passport-local-mongoose added to the project for use in authentication.

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 const games = require('./routes/games');
 const reviews = require('./routes/reviews');
@@ -42,11 +45,19 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
   res.locals.success = req.flash('success');
   res.locals.error = req.flash('error');
   next();
 });
+
 
 app.use('/games', games);
 app.use('/games/:id/reviews', reviews);
