5d9bd704696f82dba7e33e615146009a6705f049
MikeMurrey <108901311+MikeMurrey@users.noreply.github.com>
2023-04-18 16:11:35 -0600

    implement logged in and logged out behaviors
    added links for login, register, logout and only show relevant links depending on login status.  Also added middleware to track user request prior to login so as to redirect to that request upon successful login.

--- routes/users.js
+++ routes/users.js
@@ -3,18 +3,22 @@ const router = express.Router();
 const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
+const { storeReturnTo } = require('../middleware');
 
 router.get('/register', (req, res) => {
   res.render('users/register');
 });
 
-router.post('/register', catchAsync(async (req, res) => {
+router.post('/register', catchAsync(async (req, res, next) => {
   try {
     const { email, username, password } = req.body;
     const user = new User({ email, username });
     const registeredUser = await User.register(user, password);
-    req.flash('success', `Welcome to GameTracker, ${username}!`);
-    res.redirect('/games');
+    req.login(registeredUser, err => {
+      if (err) return next(err);
+      req.flash('success', `Welcome to GameTracker, ${username}!`);
+      res.redirect('/games');
+    })
   } catch (e) {
     req.flash('error', e.message);
     res.redirect('register');
@@ -25,10 +29,19 @@ router.get('/login', (req, res) => {
   res.render('users/login');
 });
 
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+router.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
   const username = req.body.username;
   req.flash('success', `Welcome back, ${username}!`);
-  res.redirect('/games');
+  const redirectUrl = res.locals.returnTo || '/games';
+  res.redirect(redirectUrl);
+});
+
+router.get('/logout', (req, res, next) => {
+  req.logout(function (err) {
+    if (err) { return next(err); }
+    req.flash('success', 'Logged Out.');
+    res.redirect('/games');
+  });
 });
 
 module.exports = router;
