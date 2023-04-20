ec37e675ade0bc5791b68f2d66b5318ad8fcf9f3
MikeMurrey <108901311+MikeMurrey@users.noreply.github.com>
2023-04-20 11:33:29 -0600

    reduced size of user and review routes
    transferred the logic inside the user and review routes to their respective controller files.

--- routes/reviews.js
+++ routes/reviews.js
@@ -5,26 +5,12 @@ const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
 
 const Game = require('../models/game');
 const Review = require('../models/review');
+const reviewController = require('../controllers/reviews');
 
 const catchAsync = require('../utils/catchAsync');
 
-router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
-  const game = await Game.findById(req.params.id);
-  const review = new Review(req.body.review);
-  review.author = req.user._id;
-  game.reviews.push(review);
-  await review.save();
-  await game.save();
-  req.flash('success', 'Posted new review!')
-  res.redirect(`/games/${game._id}`);
-}));
+router.post('/', isLoggedIn, validateReview, catchAsync(reviewController.createReview));
 
-router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(async (req, res) => {
-  const { id, reviewId } = req.params;
-  await Game.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-  await Review.findByIdAndDelete(reviewId);
-  req.flash('success', 'Review deleted successfully.')
-  res.redirect(`/games/${id}`);
-}));
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviewController.deleteReview));
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,46 +2,17 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
-const User = require('../models/user');
 const { storeReturnTo } = require('../middleware');
+const userController = require('../controllers/users');
 
-router.get('/register', (req, res) => {
-  res.render('users/register');
-});
+router.get('/register', userController.renderRegister);
 
-router.post('/register', catchAsync(async (req, res, next) => {
-  try {
-    const { email, username, password } = req.body;
-    const user = new User({ email, username });
-    const registeredUser = await User.register(user, password);
-    req.login(registeredUser, err => {
-      if (err) return next(err);
-      req.flash('success', `Welcome to GameTracker, ${username}!`);
-      res.redirect('/games');
-    })
-  } catch (e) {
-    req.flash('error', e.message);
-    res.redirect('register');
-  }
-}));
+router.post('/register', catchAsync(userController.register));
 
-router.get('/login', (req, res) => {
-  res.render('users/login');
-});
+router.get('/login', userController.renderLogin);
 
-router.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
-  const username = req.body.username;
-  req.flash('success', `Welcome back, ${username}!`);
-  const redirectUrl = res.locals.returnTo || '/games';
-  res.redirect(redirectUrl);
-});
+router.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), userController.login);
 
-router.get('/logout', (req, res, next) => {
-  req.logout(function (err) {
-    if (err) { return next(err); }
-    req.flash('success', 'Logged Out.');
-    res.redirect('/games');
-  });
-});
+router.get('/logout', userController.logout);
 
 module.exports = router;
