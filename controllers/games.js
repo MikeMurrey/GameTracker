62278eff94cdb40c8dc368ce4a7cc068d331c298
MikeMurrey <108901311+MikeMurrey@users.noreply.github.com>
2023-04-20 10:48:51 -0600

    reorganize game routes into controller file
    moved all game routes into game controller file.

---
+++ controllers/games.js
@@ -0,0 +1,55 @@
+const Game = require('../models/game');
+
+module.exports.index = async (req, res) => {
+  const games = await Game.find({});
+  res.render('games/index', { games });
+};
+
+module.exports.renderNewForm = (req, res) => {
+  res.render('games/new');
+};
+
+module.exports.createGame = async (req, res, next) => {
+  const game = new Game(req.body.game);
+  game.author = req.user._id;
+  await game.save();
+  req.flash('success', 'Game added!');
+  res.redirect(`/games/${game._id}`);
+};
+
+module.exports.showGame = async (req, res) => {
+  const game = await Game.findById(req.params.id).populate({
+    path: 'reviews',
+    populate: {
+      path: 'author'
+    }
+  }).populate('author');
+  if (!game) {
+    req.flash('error', 'Game not found!');
+    return res.redirect('/games');
+  }
+  res.render('games/show', { game });
+};
+
+module.exports.renderEditForm = async (req, res) => {
+  const game = await Game.findById(req.params.id);
+  if (!game) {
+    req.flash('error', 'Game not found!');
+    return res.redirect('/games');
+  }
+  res.render('games/edit', { game });
+};
+
+module.exports.updateGame = async (req, res) => {
+  const { id } = req.params;
+  const game = await Game.findByIdAndUpdate(id, { ...req.body.game });
+  req.flash('success', 'Game info updated!');
+  res.redirect(`/games/${game._id}`);
+};
+
+module.exports.deleteGame = async (req, res) => {
+  const { id } = req.params;
+  await Game.findByIdAndDelete(id);
+  req.flash('success', 'Deleted game successfully.')
+  res.redirect('/games');
+};
--- routes/games.js
+++ routes/games.js
@@ -1,62 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const gameController = require('../controllers/games');
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateGame } = require('../middleware');
-const Game = require('../models/game');
 
 
-router.get('/', async (req, res) => {
-  const games = await Game.find({});
-  res.render('games/index', { games });
-});
+router.get('/', catchAsync(gameController.index));
 
-router.get('/new', isLoggedIn, (req, res) => {
-  res.render('games/new');
-});
+router.get('/new', isLoggedIn, gameController.renderNewForm);
 
-router.post('/', isLoggedIn, validateGame, catchAsync(async (req, res, next) => {
-  const game = new Game(req.body.game);
-  game.author = req.user._id;
-  await game.save();
-  req.flash('success', 'Game added!');
-  res.redirect(`/games/${game._id}`);
-}));
+router.post('/', isLoggedIn, validateGame, catchAsync(gameController.createGame));
 
-router.get('/:id', catchAsync(async (req, res) => {
-  const game = await Game.findById(req.params.id).populate({
-    path: 'reviews',
-    populate: {
-      path: 'author'
-    }
-  }).populate('author');
-  if (!game) {
-    req.flash('error', 'Game not found!');
-    return res.redirect('/games');
-  }
-  res.render('games/show', { game });
-}));
+router.get('/:id', catchAsync(gameController.showGame));
 
-router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
-  const game = await Game.findById(req.params.id);
-  if (!game) {
-    req.flash('error', 'Game not found!');
-    return res.redirect('/games');
-  }
-  res.render('games/edit', { game });
-}));
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(gameController.renderEditForm));
 
-router.put('/:id', isLoggedIn, isAuthor, validateGame, catchAsync(async (req, res) => {
-  const { id } = req.params;
-  const game = await Game.findByIdAndUpdate(id, { ...req.body.game });
-  req.flash('success', 'Game info updated!');
-  res.redirect(`/games/${game._id}`);
-}));
+router.put('/:id', isLoggedIn, isAuthor, validateGame, catchAsync(gameController.updateGame));
 
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
-  const { id } = req.params;
-  await Game.findByIdAndDelete(id);
-  req.flash('success', 'Deleted game successfully.')
-  res.redirect('/games');
-}));
+router.delete('/:id', isLoggedIn, isAuthor, catchAsync(gameController.deleteGame));
 
 module.exports = router;
