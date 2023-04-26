2056384622fa02764ff0077a7226a8dba9cd42ba
MikeMurrey <108901311+MikeMurrey@users.noreply.github.com>
2023-04-26 15:25:48 -0600

    add sanitization
    add express-mongo-sanitize to project to prevent mongo injection through query strings

--- index.js
+++ index.js
@@ -13,10 +13,12 @@ const methodOverride = require('method-override');
 const passport = require('passport');
 const localStrategy = require('passport-local');
 const User = require('./models/user');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const userRoutes = require('./routes/users');
 const gameRoutes = require('./routes/games');
 const reviewRoutes = require('./routes/reviews');
+const ExpressMongoSanitize = require('express-mongo-sanitize');
 
 mongoose.set('strictQuery', true);
 mongoose.connect('mongodb://localhost:27017/game-tracker');
@@ -36,6 +38,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize());
 
 const sessionConfig = {
   secret: 'thisshouldbeabettersecret',
@@ -58,6 +61,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
+  console.log(req.query);
   res.locals.currentUser = req.user;
   res.locals.success = req.flash('success');
   res.locals.error = req.flash('error');
